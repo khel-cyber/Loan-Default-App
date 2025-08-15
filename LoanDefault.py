902ea20fd154f0b8820e914f73ee8ab95747e97c
khel-cyber <yeboahkelvin2004@gmail.com>
2025-08-15 13:36:01 +0000

    Add files via upload

--- LoanDefault.py
+++ LoanDefault.py
@@ -23,7 +23,7 @@ st.set_page_config(page_title="Loan Default Prediction (Naïve Bayes & Decision 
 
 # -------------------- Load dataset directly --------------------
 DATA_PATH = 'C:/Users/khelv/PycharmProjects/PythonProject1/train_u6lujuX_CVtuZ9i (1).csv'
-df_loaded = pd.read_csv(DATA_PATH)
+df_loaded = pd.read_csv('train_u6lujuX_CVtuZ9i (1)')
 
 # Keep only relevant columns (if present)
 keep = ['Loan_ID','Gender','Married','Dependents','Education','Self_Employed',
